3579148c14e82a6ba62aad2f175c6753f8b9ee1e
Azizbek Sobirjonov <azizbeksobirjonov93796@gmail.com>
2026-07-09 13:09:16 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,86 @@
+class Salom:
+    def info(self):
+        print("Bu class ekranga 'Salom Dunyo' matnini chiqaradi.")
+
+s = Salom()
+s.info()
+
+
+class Oquvchi:
+    def info(self):
+        print("Bu class o'quvchining ismini chiqaradi.")
+
+o = Oquvchi()
+o.info()
+
+
+class Mashina:
+    def info(self):
+        print("Bu class mashina nomini chiqaradi.")
+
+m = Mashina()
+m.info()
+
+
+class Telefon:
+    def info(self):
+        print("Bu class telefon nomini chiqaradi.")
+
+t = Telefon()
+t.info()
+
+
+class Hayvon:
+    def info(self):
+        print("Bu class hayvonning ovozini chiqaradi.")
+
+h = Hayvon()
+h.info()
+
+
+class Son:
+    def info(self):
+        print("Bu class 1 dan 5 gacha sonlarni chiqaradi.")
+
+s = Son()
+s.info()
+
+
+class Juft:
+    def info(self):
+        print("Bu class juft sonlarni chiqaradi.")
+
+j = Juft()
+j.info()
+
+
+class Toq:
+    def info(self):
+        print("Bu class toq sonlarni chiqaradi.")
+
+t = Toq()
+t.info()
+
+
+class Hisob:
+    def info(self):
+        print("Bu class ikki sonni qo'shadi.")
+
+h = Hisob()
+h.info()
+
+
+class Kvadrat:
+    def info(self):
+        print("Bu class sonning kvadratini hisoblaydi.")
+
+k = Kvadrat()
+k.info()
+
+
+class Dastur:
+    def info(self):
+        print("Bu class dastur ishga tushganini bildiradi.")
+
+d = Dastur()
+d.info()
